981eca4e05e70976c3c26789cc3e942a9f1212d0
BeetleChunks <robert.mcdown@gmail.com>
2019-05-31 23:42:07 -0500

    Bug fixes and features added.

--- mojibake/__init__.py
+++ mojibake/__init__.py
@@ -16,39 +16,39 @@ class Mojibake:
 	# If string then it will be decoded then re-encoded
 	def encode(self, unicode_data, preserve_type=False):
 		try:
-			if type(unicode_data) == unicode:
+			if isinstance(unicode_data, unicode):
 				string_data = unicode_data.encode(self.default_encoding, self.default_error_handling)
 
 				return string_data
 
-			elif type(unicode_data) == str:
+			elif isinstance(unicode_data, str):
 				string_data = self.decode(unicode_data)
 				string_data = string_data.encode(self.default_encoding, self.default_error_handling)
 
 				return string_data
 
-			elif type(unicode_data) == int:
+			elif isinstance(unicode_data, int):
 				string_data = str(unicode_data)
 				string_data = self.decode(string_data)
 				string_data = string_data.encode(self.default_encoding, self.default_error_handling)
 
 				return string_data
 
-			elif type(unicode_data) == long:
+			elif isinstance(unicode_data, long):
 				string_data = str(unicode_data)
 				string_data = self.decode(string_data)
 				string_data = string_data.encode(self.default_encoding, self.default_error_handling)
 
 				return string_data
 
-			elif type(unicode_data) == float:
+			elif isinstance(unicode_data, float):
 				string_data = str(unicode_data)
 				string_data = self.decode(string_data)
 				string_data = string_data.encode(self.default_encoding, self.default_error_handling)
 
 				return string_data
 
-			elif type(unicode_data) == complex:
+			elif isinstance(unicode_data, complex):
 				string_data = str(unicode_data)
 				string_data = self.decode(string_data)
 				string_data = string_data.encode(self.default_encoding, self.default_error_handling)
@@ -62,7 +62,7 @@ class Mojibake:
 				return string_data
 
 			# Needs testing and validation
-			elif type(unicode_data) == bytearray:
+			elif isinstance(unicode_data, bytearray):
 				string_data = self.decode(unicode_data)
 				string_data = string_data.encode(self.default_encoding, self.default_error_handling)
 
@@ -70,7 +70,14 @@ class Mojibake:
 
 			# Placeholder for now
 			else:
-				raise ValueError("%s is an invalid type for encode()" % (type(unicode_data)))
+				try:
+					string_data = unicode_data.encode(self.default_encoding, self.default_error_handling)
+
+					return string_data
+
+				except Exception as e:
+					log.error("Mojibake Error: %s", e)
+					raise ValueError("%s is an invalid type for encode()" % (type(unicode_data)))
 
 		except Exception as e:
 			log.exception("%s", e, exc_info=True)
@@ -79,36 +86,36 @@ class Mojibake:
 	# String to unicode; unicode WILL be returned regardless of type by default
 	def decode(self, string_data, preserve_type=False):
 		try:
-			if type(string_data) == str:
+			if isinstance(string_data, str):
 				unicode_data = normalize(self.default_normalizer, string_data.decode(self.default_encoding, self.default_error_handling))
 
 				return unicode_data
 
-			elif type(string_data) == unicode:
+			elif isinstance(string_data, unicode):
 				unicode_data = self.encode(string_data)
 				unicode_data = normalize(self.default_normalizer, unicode_data.decode(self.default_encoding, self.default_error_handling))
 
 				return unicode_data
 
-			elif type(string_data) == int:
+			elif isinstance(string_data, int):
 				unicode_data = str(string_data)
 				unicode_data = normalize(self.default_normalizer, unicode_data.decode(self.default_encoding, self.default_error_handling))
 
 				return unicode_data
 
-			elif type(string_data) == long:
+			elif isinstance(string_data, long):
 				unicode_data = str(string_data)
 				unicode_data = normalize(self.default_normalizer, unicode_data.decode(self.default_encoding, self.default_error_handling))
 
 				return unicode_data
 
-			elif type(string_data) == float:
+			elif isinstance(string_data, float):
 				unicode_data = str(string_data)
 				unicode_data = normalize(self.default_normalizer, unicode_data.decode(self.default_encoding, self.default_error_handling))
 
 				return unicode_data
 
-			elif type(string_data) == complex:
+			elif isinstance(string_data, complex):
 				unicode_data = str(string_data)
 				unicode_data = normalize(self.default_normalizer, unicode_data.decode(self.default_encoding, self.default_error_handling))
 
@@ -120,14 +127,21 @@ class Mojibake:
 				return unicode_data
 
 			# Needs testing and validation
-			elif type(string_data) == bytearray:
+			elif isinstance(string_data, bytearray):
 				unicode_data = normalize(self.default_normalizer, string_data.decode(self.default_encoding, self.default_error_handling))
 
 				return unicode_data
 
 			# Placeholder for now
 			else:
-				raise ValueError("%s is an invalid type for encode()" % (type(string_data)))
+				try:
+					unicode_data = normalize(self.default_normalizer, string_data.decode(self.default_encoding, self.default_error_handling))
+
+					return unicode_data
+
+				except Exception as e:
+					log.error("Mojibake Error: %s", e)
+					raise ValueError("%s is an invalid type for decode()" % (type(string_data)))
 
 		except Exception as e:
 			log.exception("%s", e, exc_info=True)
@@ -136,28 +150,26 @@ class Mojibake:
 	# Itters a dict to encode all unicode values to strings
 	# If string value is observed, it will be decoded then re-encoded
 	def dict_encode(self, dict_obj):
+		new_dict = dict()
+
 		try:
 			if hasattr(dict_obj, 'iteritems'):
 				for nKey, nData in dict_obj.iteritems():
-					if type(nData) == unicode:
-						dict_obj[nKey] = self.encode(nData)
+					new_nKey = self.encode(nKey)
 
-					elif type(nData) == str:
-						dict_obj[nKey] = self.encode(nData)
+					if isinstance(nData, dict):
+						new_dict[new_nKey] = self.dict_encode(nData)
 
-					elif isinstance(nData, dict):
-						dict_obj[nKey] = self.dict_encode(nData)
-					
+					elif isinstance(nData, tuple):
+						new_dict[new_nKey] = self.tuple_encode(nData)
+
 					elif isinstance(nData, list):
-						for i in range(0, len(nData)):
-							nData[i] = self.dict_encode(nData[i])
+						new_dict[new_nKey] = self.list_encode(nData)
 
-						dict_obj[nKey] = nData
+					else:
+						new_dict[new_nKey] = self.encode(nData)
 
-			else:
-				dict_obj = self.encode(dict_obj)
-
-			return dict_obj
+			return new_dict
 							
 		except Exception as e:
 			log.exception("%s", e, exc_info=True)
@@ -165,28 +177,26 @@ class Mojibake:
 
 	# Itters a dict to decode all string values to unicode
 	def dict_decode(self, dict_obj):
+		new_dict = dict()
+
 		try:
 			if hasattr(dict_obj, 'iteritems'):
 				for nKey, nData in dict_obj.iteritems():
-					if type(nData) == unicode:
-						dict_obj[nKey] = self.decode(nData)
+					new_nKey = self.decode(nKey)
 
-					elif type(nData) == str:
-						dict_obj[nKey] = self.decode(nData)
+					if isinstance(nData, dict):
+						new_dict[new_nKey] = self.dict_decode(nData)
 
-					elif isinstance(nData, dict):
-						dict_obj[nKey] = self.dict_decode(nData)
-					
+					elif isinstance(nData, tuple):
+						new_dict[new_nKey] = self.tuple_decode(nData)
+
 					elif isinstance(nData, list):
-						for i in range(0, len(nData)):
-							nData[i] = self.dict_decode(nData[i])
+						new_dict[new_nKey] = self.list_decode(nData)
 
-						dict_obj[nKey] = nData
+					else:
+						new_dict[new_nKey] = self.decode(nData)
 
-			else:
-				dict_obj = self.decode(dict_obj)
-
-			return dict_obj
+			return new_dict
 							
 		except Exception as e:
 			log.exception("%s", e, exc_info=True)
@@ -195,19 +205,24 @@ class Mojibake:
 	# Itters a list to encode all unicode values to strings
 	# If string value is observed, it will be decoded then re-encoded
 	def list_encode(self, list_obj):
+		new_list = list()
+
 		try:
-			if type(list_obj) == list:
+			if isinstance(list_obj, list):
 				for i in range(0, len(list_obj)):
 					if hasattr(list_obj[i], 'iteritems'):
-						list_obj[i] = self.dict_encode(list_obj[i])
+						new_list.append(self.dict_encode(list_obj[i]))
 
-					elif type(list_obj[i]) == list:
-						list_obj[i] = self.list_encode(list_obj[i])
+					elif isinstance(list_obj[i], list):
+						new_list.append(self.list_encode(list_obj[i]))
+
+					elif isinstance(list_obj[i], tuple):
+						new_list.append(self.tuple_encode(list_obj[i]))
 
 					else:
-						list_obj[i] = self.encode(list_obj[i])
+						new_list.append(self.encode(list_obj[i]))
 
-			return list_obj
+			return new_list[::]
 							
 		except Exception as e:
 			log.exception("%s", e, exc_info=True)
@@ -215,19 +230,89 @@ class Mojibake:
 
 	# Itters a list to decode all string values to unicode
 	def list_decode(self, list_obj):
+		new_list = list()
+
 		try:
-			if type(list_obj) == list:
+			if isinstance(list_obj, list):
 				for i in range(0, len(list_obj)):
 					if hasattr(list_obj[i], 'iteritems'):
-						list_obj[i] = self.dict_decode(list_obj[i])
+						new_list.append(self.dict_decode(list_obj[i]))
 
-					elif type(list_obj[i]) == list:
-						list_obj[i] = self.list_decode(list_obj[i])
+					elif isinstance(list_obj[i], list):
+						new_list.append(self.list_decode(list_obj[i]))
+
+					elif isinstance(list_obj[i], tuple):
+						new_list.append(self.tuple_decode(list_obj[i]))
 
 					else:
-						list_obj[i] = self.decode(list_obj[i])
+						new_list.append(self.decode(list_obj[i]))
 
-			return list_obj
+			return new_list[::]
+							
+		except Exception as e:
+			log.exception("%s", e, exc_info=True)
+			raise
+
+	# Itters a tuple to decode all unicode values to strings
+	# If string value is observed, it will be decoded then re-encoded
+	def tuple_encode(self, tuple_obj):
+		# Because a tuple is immutable, we have to create a new list
+		# and append the tuple data to it after modification then 
+		# add it to the new tuple
+		new_list  = list()
+
+		try:
+			if isinstance(tuple_obj, tuple):
+				for i in range(0, len(tuple_obj)):
+					if hasattr(tuple_obj[i], 'iteritems'):
+						new_list.append(self.dict_encode(tuple_obj[i]))
+
+					elif isinstance(tuple_obj[i], list):
+						new_list.append(self.list_encode(tuple_obj[i]))
+
+					elif isinstance(tuple_obj[i], tuple):
+						new_list.append(self.tuple_encode(tuple_obj[i]))
+
+					else:
+						new_list.append(self.encode(tuple_obj[i]))
+
+				new_tuple = tuple(new_list)
+				return new_tuple
+
+			else:
+				return tuple_obj
+							
+		except Exception as e:
+			log.exception("%s", e, exc_info=True)
+			raise
+
+	# Itters a tuple to decode all string values to unicode
+	def tuple_decode(self, tuple_obj):
+		# Because a tuple is immutable, we have to create a new list
+		# and append the tuple data to it after modification then 
+		# add it to the new tuple
+		new_list  = list()
+
+		try:
+			if isinstance(tuple_obj, tuple):
+				for i in range(0, len(tuple_obj)):
+					if hasattr(tuple_obj[i], 'iteritems'):
+						new_list.append(self.dict_decode(tuple_obj[i]))
+
+					elif isinstance(tuple_obj[i], list):
+						new_list.append(self.list_decode(tuple_obj[i]))
+
+					elif isinstance(tuple_obj[i], tuple):
+						new_list.append(self.tuple_decode(tuple_obj[i]))
+
+					else:
+						new_list.append(self.decode(tuple_obj[i]))
+
+				new_tuple = tuple(new_list)
+				return new_tuple
+
+			else:
+				return tuple_obj
 							
 		except Exception as e:
 			log.exception("%s", e, exc_info=True)
@@ -235,7 +320,7 @@ class Mojibake:
 
 	# Serializes dict to string using json.dumps AFTER dict_encoding the dictionary
 	def dict_to_string(self, dict_obj):
-		assert type(dict_obj) == dict
+		assert isinstance(dict_obj, dict)
 
 		dict_obj = self.dict_encode(dict_obj)
 		serialized_dict = json.dumps(dict_obj)
@@ -244,7 +329,7 @@ class Mojibake:
 
 	# deserializes string to dict object using json.loads AFTER encoding the string
 	def string_to_dict(self, serialized_dict):
-		assert type(serialized_dict) == str or type(serialized_dict) == unicode
+		assert isinstance(dict_obj, str) or isinstance(dict_obj, unicode)
 
 		serialized_dict = self.encode(serialized_dict)
 		dict_obj = json.loads(serialized_dict)
@@ -254,7 +339,7 @@ class Mojibake:
 	# returns url decoded STRING
 	def decode_url(self, string_data):
 		try:
-			if type(string_data) == str or type(string_data) == unicode:
+			if isinstance(dict_obj, str) or isinstance(dict_obj, unicode):
 				string_data    = self.encode(string_data)
 				encoded_string = urllib.unquote_plus(string_data)
 
@@ -270,7 +355,7 @@ class Mojibake:
 	# returns url encoded STRING
 	def encode_url(self, string_data):
 		try:
-			if type(string_data) == str or type(string_data) == unicode:
+			if isinstance(dict_obj, str) or isinstance(dict_obj, unicode):
 				string_data    = self.encode(string_data)
 				encoded_string = urllib.quote_plus(string_data)
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='mojibake',
-	version='0.0.1',
+	version='0.0.2',
 	description='Library of data encoding/decoding utility functions',
 	license='GPLv3+',
 	packages=['mojibake'],
